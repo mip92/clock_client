144de4fdd551b523df91f05ca764209d9e362e29
mip92 <75443445+mip92@users.noreply.github.com>
2022-05-30 14:21:21 +0300

    Update adminCityActionCreators.ts

--- src/actionCreators/adminCityActionCreators.ts
+++ src/actionCreators/adminCityActionCreators.ts
@@ -97,8 +97,10 @@ export const addOneCity = (city: string, price: number) => {
         } catch (e) {
             dispatch(fetchStart(false))
             let error: MyError
-            if (JSON.parse(e.request.responseText)?.hasOwnProperty('errors')===true)  error = JSON.parse(e.request.responseText).errors[0]
-            else error = JSON.parse(e.request.responseText)
+            JSON.parse(e.request.responseText)?.hasOwnProperty('errors') ?
+                error = JSON.parse(e.request.responseText).errors[0]
+                :
+                error = JSON.parse(e.request.responseText)
             dispatch(fetchError(error))
         }
     }
